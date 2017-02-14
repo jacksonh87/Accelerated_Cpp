afa0ad94e5fc96910f901c4eb09b6396bffc1eda
Jackson <jackson.d.hunter@gmail.com>
2017-02-14 23:28:55 +0000

    First commit

---
+++ Chapter00/001_Hello_world/001_Hello_world/001_Hello_world.cpp
@@ -0,0 +1,12 @@
+// 001_Hello_world.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+// a small C++ program
+#include <iostream>
+int main()
+{
+	std::cout << "Hello, world!" << std::endl;
+	return 0;
+}
