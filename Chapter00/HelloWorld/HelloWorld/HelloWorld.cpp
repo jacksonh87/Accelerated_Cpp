882c22a3e145bd871121e0f0491e0ea985133b5d
Jackson <jackson.d.hunter@gmail.com>
2017-02-14 23:42:29 +0000

    First commit

---
+++ Chapter00/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,15 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+
+
+
+
+
+#include "stdafx.h"
+
+
+#include <iostream>
+int main()
+{
+	std::cout << "Hello, world!" << std::endl;
+	return 0;
+}
